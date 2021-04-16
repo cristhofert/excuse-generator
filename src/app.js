e50e11639f97274bcd9f05eb6eb759f622556a23
cristhofert <cristhofert97@gmail.com>
2021-04-16 13:05:05 +0000

    Ready

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * 4);
+  let actionIndex = Math.floor(Math.random() * 4);
+  let whatIndex = Math.floor(Math.random() * 3);
+  let whenIndex = Math.floor(Math.random() * 5);
+
+  console.log(
+    who[whoIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex]
+  );
 };
